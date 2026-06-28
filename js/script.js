aba81680aad0c53b3bcc884c9063b1ca8b60b954
ayeshasiddikaoma-cmyk <ayeshasiddikaoma@gmail.com>
2026-06-28 20:33:54 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$('.sliders').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  arrow:false,
+  prevArrow:'<i class="fa-solid fa-angles-left left"></i>',
+nextArrow:'<i class="fa-solid fa-angles-right right" ></i>',
+
+});
